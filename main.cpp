512382b1dbf7fc084b9ffa43d971f49243badbc0
mmk <mmkkarbalaei@gmail.com>
2024-02-09 23:00:29 +0330

    create base structure of project

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <unordered_set>
+#include <iomanip>
+#include <memory>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
